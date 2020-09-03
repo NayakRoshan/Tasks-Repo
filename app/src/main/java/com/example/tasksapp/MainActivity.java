9eb8cab255c004e3cc51448c69bdf631e43e3d72
Roshan Nayak <roshannayak610@gmail.com>
2020-09-03 16:00:21 +0530

    task_4 done

--- app/src/main/java/com/example/tasksapp/MainActivity.java
+++ app/src/main/java/com/example/tasksapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tasksapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("test1", "in onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("test1", "in onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("test1", "on Resume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("test1", "on stop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("test1", "on pause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("test1", "on destroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("test1", "on restart");
     }
 }
