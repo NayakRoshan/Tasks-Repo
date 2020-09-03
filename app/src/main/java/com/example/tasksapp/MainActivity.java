7928e32403ee48b1cdec0c8af05fd55baa5de52a
Roshan Nayak <roshannayak610@gmail.com>
2020-09-03 17:59:55 +0530

    task_7 done

--- app/src/main/java/com/example/tasksapp/MainActivity.java
+++ app/src/main/java/com/example/tasksapp/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.tasksapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +17,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction addFragmentOne = fragmentManager.beginTransaction();
+        Fragment fragment1 = new FirstFragment();
+        addFragmentOne.add(R.id.fragment, fragment1);
+        addFragmentOne.commit();
+
+
         Log.d("test1", "in onCreate");
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction toggleFragments = fragmentManager.beginTransaction();
+
+                Fragment fragment = fragmentManager.findFragmentById(R.id.fragment);
+
+                if (fragment instanceof FirstFragment) {
+                    Fragment fragment1 = new SecondFragment();
+                    toggleFragments.replace(R.id.fragment, fragment1);
+                    toggleFragments.addToBackStack(null);
+                } else {
+                    Fragment fragment1 = new FirstFragment();
+                    toggleFragments.replace(R.id.fragment, fragment1);
+                    toggleFragments.addToBackStack(null);
+                }
+                toggleFragments.commit();
+            }
+        });
     }
 
     @Override
